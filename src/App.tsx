db0fed7992ddfbbf204a96da287ae2b17952446e
ns-adiraghavan <adi.raghavan@netscribes.com>
2026-09-04 20:00:25 +0530

    upload: src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { AppProvider, useApp } from './app-context';
+import { Sidebar } from './components/Sidebar';
+import { Dashboard } from './screens/Dashboard';
+import { Requirements } from './screens/Requirements';
+import { RequirementDetail } from './screens/RequirementDetail';
+import { Vendors } from './screens/Vendors';
+import { VendorUpload } from './screens/VendorUpload';
+import { RateCard } from './screens/RateCard';
+import { Generate } from './screens/Generate';
+
+function Screen() {
+  const { route } = useApp();
+  switch (route.name) {
+    case 'requirements': return <Requirements />;
+    case 'reqDetail': return <RequirementDetail />;
+    case 'vendors': return <Vendors />;
+    case 'upload': return <VendorUpload />;
+    case 'ratecard': return <RateCard />;
+    case 'generate': return <Generate />;
+    default: return <Dashboard />;
+  }
+}
+
+export default function App() {
+  return (
+    <AppProvider>
+      <div className="shell">
+        <Sidebar />
+        <div className="main">
+          <Screen />
+        </div>
+      </div>
+    </AppProvider>
+  );
+}
